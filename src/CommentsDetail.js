74086bd4618378a835ffb68905754c5791692de5
ManikandanRamSaro <mani95ram@outlook.com>
2020-12-28 20:50:10 +0530

    First commit  - working on comments using components - 28-12-2020

---
+++ src/CommentsDetail.js
@@ -0,0 +1,26 @@
+import React from 'react'; 
+
+import faker from 'faker';
+const CommentsDetail = (props) =>
+{ 
+    return (
+        <div className="comment">
+          <a href="/" className="avatar">
+             <img alt="avatar" src={props.images}/>
+          </a>
+       
+          <div className="content">
+              <a href="/" className="author">
+                 {props.author}
+              </a>
+              <div className="metadata"> 
+                <span className="date">{props.timeat}</span>
+              </div>
+              <div className="text">
+                {props.comments}
+              </div>
+          </div>
+        </div>
+    );
+}
+export default CommentsDetail;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom'; 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import faker from 'faker';
+import CommentsDetail from './CommentsDetail';
+const App = () =>{
+  return (
+    <div className="ui container comments">
+        
+        <CommentsDetail author="Mani" timeat="Today 7:41 PM" comments="this is sample comment one" images={faker.image.image()}/>
+        <CommentsDetail author="Durai" timeat="Today 10:41 AM" comments="this is sample comment two"  images={faker.image.image()}/>
+        <CommentsDetail author="Shree" timeat="Yesterday 7:41 PM" comments="this is sample comment three"  images={faker.image.image()}/>
+    </div>
+  );
+}
+ReactDOM.render( 
+    <App /> ,
+  document.querySelector('#root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ 
