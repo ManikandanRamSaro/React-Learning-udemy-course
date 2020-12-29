7b36533877419ef21cb70e03afe27c91f79facb9
ManikandanRamSaro <mani95ram@outlook.com>
2020-12-29 12:36:49 +0530

    After Function based react learning done - 29-12-2020

--- src/index.js
+++ src/index.js
@@ -7,11 +7,17 @@ import ApprovalCord from './ApprovalCord';
 const App = () =>{
   return (
     <div className="ui container comments">
+       <ApprovalCord>Are you sure you happy to learn it</ApprovalCord>
         <ApprovalCord>
-        <CommentsDetail author="Mani" timeat="Today 7:41 PM" comments="this is sample comment one" images={faker.image.image()}/>
+            <CommentsDetail author="Mani" timeat="Today 7:41 PM" comments="this is sample comment one" images={faker.image.image()}/>
         </ApprovalCord>
-        <CommentsDetail author="Durai" timeat="Today 10:41 AM" comments="this is sample comment two"  images={faker.image.image()}/>
-        <CommentsDetail author="Shree" timeat="Yesterday 7:41 PM" comments="this is sample comment three"  images={faker.image.image()}/>
+        <ApprovalCord>
+            <CommentsDetail author="Durai" timeat="Today 10:41 AM" comments="this is sample comment two"  images={faker.image.image()}/>
+        </ApprovalCord>
+        <ApprovalCord>
+            <CommentsDetail author="Shree" timeat="Yesterday 7:41 PM" comments="this is sample comment three"  images={faker.image.image()}/>
+        </ApprovalCord>
+        
     </div>
   );
 }
